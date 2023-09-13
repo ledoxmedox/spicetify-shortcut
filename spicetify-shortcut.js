821bd5dc55fa4f34a29d4d88c84bfe80d14513a8
ledox <101424523+ledoxmedox@users.noreply.github.com>
2023-09-13 17:46:14 +0700

    Update spicetify-shortcut.js

--- spicetify-shortcut.js
+++ spicetify-shortcut.js
@@ -1,26 +1,10 @@
-// https://github.com/spicetify/spicetify-cli/blob/master/globals.d.ts
-
-function eventTriggerWithKey(key, functionToExecute) {
-Spicetify.Mousetrap.bind(key, functionToExecute)
-}
-
-eventTriggerWithKey("ctrl+shift+up", () => {
-Spicetify.Player.setVolume(100);
-})
-
-eventTriggerWithKey("ctrl+shift+down", () => {
-Spicetify.Player.setVolume(0);
-})
-
-eventTriggerWithKey("l", () => {
-document.querySelector("button[aria-label='Lyrics']").click();
-})
-
-eventTriggerWithKey("f11", () => {
-document.querySelector("button[aria-label='Full screen']").click();
-document.querySelector("button[aria-label='Exit full screen']").click();
-})
-
-eventTriggerWithKey("q", () => {
-document.querySelector("button[aria-label='Queue']").click();
-})
+document.addEventListener("keydown", (event) => {
+  if (event.ctrlKey && event.shiftKey && event.key === "ArrowUp") {
+    Spicetify.Player.setVolume(100);
+  } else if (event.ctrlKey && event.shiftKey && event.key === "ArrowDown") {
+    Spicetify.Player.setVolume(0);
+  } else if (event.key === "l") {
+    document.querySelector("button[aria-label='Lyrics']").click();
+  }
+  // Add more shortcuts here
+});
